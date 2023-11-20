3e23e27a4de3c5a790d8b18b1732c868cfb98a09
Javier Ramirez <jrstst40@gmail.com>
2023-11-20 13:16:00 -0700

    hi

--- AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/BACKUP1.m
+++ AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/BACKUP1.m
@@ -1,56 +1,46 @@
-while True
-    brick.SetColorMode(1,1);
+brick.SetColorMode(1,1);
+
+while 1
     color = brick.ColorCode(1);
     dist = brick.UltrasonicDist(2);
-
-    threshold = 50;
+    touch = brick.TouchPressed(3);
 
     brick.MoveMotor('AB', 50);
 
-    if ((color == 2) || (color == 5)) 
-        brick.pause(3);
+    if color == 5
+        disp('red');
+        brick.StopMotor('AB', 'Brake');
+        pause(3);
         brick.beep();
-        display(color);
     end
     if ((color == 3) || (color == 4))
         brick.pause(3);
         brick.beep();
-        display(color);
+        disp(color);
     end
     if dist < 1
+        pause(0.5);
         brick.beep();
+        brick.StopMotor('AB', 'Brake');
+        brick.MoveMotor('AB', -40);
+        pause(2.3);
         brick.MoveMotorAngleAbs('A', 50, 180, 'Coast');
         brick.WaitForMotor('A');
         brick.MoveMotor('AB', 50);
+        pause(2);
+    end
         
-        if dist < 3
+    if ((dist < 3)&& (dist > 1))
             brick.beep();
             brick.MoveMotorAngleAbs('A', 50, -90, 'Coast');
             brick.WaitForMotor('A');
             brick.MoveMotor('AB', 50);
-        
-        else
-            brick.pause(2);
-            brick.beep();
-            brick.MoveMotorAngleAbs('A', 50, 90, 'Coast');
-            brick.WaitForMotor('A');
-            brick.MoveMotor('AB', 50);
-            if dist < 1
-                brick.MoveMotor('AB', -10);
-                brick.pause(1);
-                brick.MoveMotorAngleRel('A', 50, 20, "Coast");
-                brick.WaitForMotor('A');
-                brick.MoveMotor('AB', 50);
-                brick.pause(2);
-                brick.StopMotor('AB');
-            end
-        end
 
     end
     
-    if dist > threshold
+    if dist < 0.5
         pause(0.8);
-        brick.MoveMotor('AB', -10)
+        brick.MoveMotor('AB', -40)
         brick.WaitForMotor('AB');
         pause(0.2);
         brick.MoveMotorAngleAbs('A', 30, -90);
