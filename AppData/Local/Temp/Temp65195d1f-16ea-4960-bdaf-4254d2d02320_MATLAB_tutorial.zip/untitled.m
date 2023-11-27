e7159dc2a2e52c84f93251dadced7d676d9400cc
Javier Ramirez <jrstst40@gmail.com>
2023-11-27 14:31:09 -0700

    hi

--- AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/untitled.m
+++ AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/untitled.m
@@ -1,31 +1,51 @@
-try
-    brick.SetColorMode(1, 1);
-    color = brick.ColorCode(1);
-    dist = brick.UltrasonicDist(2);
+brick.SetColorMode(4, 2);
 
-    brick.MoveMotor('AB', 50);
-
-    if ((color == 2) || (color == 5))
-        brick.beep();
-        display(color);
-        brick.stopMotor('AB', 'Coast');
-    end
-    
-    if color == 4
-        brick.beep();
-        brick.MoveMotorAngleAbs('A', 50, 90, 'Coast');
-        brick.WaitForMotor();
-        brick.MoveMotor('AB', 50);
-        if dist < 1
-            brick.beep();
-            brick.MoveMotorAngleAbs('A', 50, 180, 'Coast');
-            brick.WaitForMotor();
-            brick.MoveMotor('AB', 50);
-            color = brick.ColorCode(1);
-        end
-    end 
-catch
-    brick.beep();
-    brick.beep();
+while 1
+    brick.MoveMotorAngleRel('B', 30, 90);
+    brick.WaitForMotor('B');
+    brick.MoveMotor('AB', 30);
+    pause(1.5);
     brick.StopMotor('AB');
+    brick.MoveMotorAngleRel('A', 30, 90);
+    brick.WaitForMotor('A');
+    pause(1);
+    % Alternate logic for turning
+
+    if dist < 4
+        pause(1);
+        brick.StopAllMotors('brake');
+        brick.MoveMotorAngleRel('B', 30, 90);
+        brick.WaitForMotor('B');
+        brick.StopMotor('B');
+        brick.MoveMotor('AB', 30);
+        pause(1.5);
+    end
+    brick.StopAllMotors('brake');
+    brick.MoveMotorAngleRel('B', 30, 90);
+    brick.WaitForMotor('B');
+    pause(1);
+    brick.MoveMotor('AB', 20);
+
+    if color == 4 || color == 5 || color == 3
+        brick.StopAllMotors('brake');
+        run('keycontrol.m');
+        pause(5);
+    end
+
+    brick.StopMotor('A');
+    brick.MoveMotor('AB', 30);
+    pause(1.5);
+    brick.StopMotor('AB');
+    brick.MoveMotor('A', 30, 90)
+
+    if dist > 4
+        pause(2);
+        brick.MoveMotorAngleRel('A', 30, 90);
+        brick.WaitForMotor('A');
+        pause(1);
+        brick.StopAllMotors('brake');
+        pause(2);
+        brick.MoveMotor('AB', 30);
+    end
+
 end
