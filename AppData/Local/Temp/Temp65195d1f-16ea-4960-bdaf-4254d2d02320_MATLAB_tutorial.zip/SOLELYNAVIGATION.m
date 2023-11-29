0456f8f37fbeee9c4331462978e0e020b2a6ec87
Javier Ramirez <jrstst40@gmail.com>
2023-11-28 18:05:33 -0700

    hi

--- AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
+++ AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
@@ -1,35 +1,26 @@
 % FIGURE OUT WHICH MOTORS TO TURN AND HOW LONG
+brick.SetColorMode(4, 2);
+
 
 while 1
     dist = brick.UltrasonicDist(1);
     touched = brick.TouchPressed(2);
+    color = brick.ColorCode(4);
 
     % START
     % TURN LEFT
-    brick.MoveMotorAngleRel('B', 30, 90);
-    brick.WaitForMotor('B');
     brick.MoveMotor('AB', 30);
-    pause(1.5);
 
-    % MOVE FORWARD, STOP AND TURN RIGHT
-    brick.StopMotor('AB');
-    brick.MoveMotorAngleRel('A', 30, 90);
-    brick.WaitForMotor('A');
-    pause(1);
+    if color == 4 || color == 5 || color == 3
+        brick.beep();
+        brick.StopMotor('AB');
+        run('keycontrol.m');
+        % BEFORE ENDING KEY, ORIENT MOTOR PROPERLY
+        pause(3);
+    end
 
-    % MOVE FORWARD AND TURN LEFT
     brick.MoveMotor('AB', 30);
-    pause(1.5);
-    brick.StopMotor('AB');
-
-    brick.MoveMotorAngleRel('B', 30, 90);
-    brick.WaitForMotor('B');
-    pause(1);
-
-    % MOVE UNTIL REACH YELLER
-    brick.MoveMotor('AB', 20);
-    pause(1.5);
-
+    
     if touched 
         brick.StopMotor('AB');
         brick.MoveMotor('AB', -30);
@@ -41,14 +32,11 @@ while 1
         pause(4);
     end
 
+
+    
+
     % MANUAL CONTROL
-    if color == 4 || color == 5 || color == 3
-        brick.beep();
-        brick.StopMotor('AB');
-        run('keycontrol.m');
-        % BEFORE ENDING KEY, ORIENT MOTOR PROPERLY
-        pause(5);
-    end
+    
 
     % LEAVING YELLER
     brick.MoveMotor('AB', 30);
