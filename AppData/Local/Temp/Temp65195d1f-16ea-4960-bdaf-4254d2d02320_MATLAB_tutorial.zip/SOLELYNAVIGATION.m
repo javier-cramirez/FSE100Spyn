967f676525da0a20d72514c627e18b2f0bf3b5bd
Javier Ramirez <jrstst40@gmail.com>
2023-11-28 18:42:10 -0700

    new

--- AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
+++ AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
@@ -1,6 +1,5 @@
-% FIGURE OUT WHICH MOTORS TO TURN AND HOW LONG
+% FIGURE OUT WHICH MOTORS T TURN AND HOW LONG
 brick.SetColorMode(4, 2);
-
 
 while 1
     dist = brick.UltrasonicDist(1);
@@ -9,9 +8,9 @@ while 1
 
     % START
     % TURN LEFT
-    brick.MoveMotor('AB', 30);
+    brick.MoveMotor('AB', 60);
 
-    if color == 4 || color == 5 || color == 3
+    if (color == 4 || color == 5 || color == 3 && color ~= 1 && color ~=0)
         brick.beep();
         brick.StopMotor('AB');
         run('keycontrol.m');
@@ -19,17 +18,16 @@ while 1
         pause(3);
     end
 
-    brick.MoveMotor('AB', 30);
+    brick.MoveMotor('AB', 60);
     
     if touched 
         brick.StopMotor('AB');
-        brick.MoveMotor('AB', -30);
+        brick.MoveMotor('AB', -60);
         pause(1);
         brick.StopMotor('AB');
-        brick.MoveMotorAngleRel('B', 30, 90);
+        brick.MoveMotorAngleRel('B', 60, 150);
         brick.WaitForMotor('B');
-        brick.MoveMotor('AB', 30);
-        pause(4);
+        brick.MoveMotor('AB', 60);
     end
 
 
@@ -39,25 +37,25 @@ while 1
     
 
     % LEAVING YELLER
-    brick.MoveMotor('AB', 30);
+    brick.MoveMotor('AB', 55);
 
     % HOW TO LEAVE?
     % WHEN WE REACH LAST WALL OUT, TURN LEFT
     if dist < 4
         pause(3);
-        brick.MoveMotorAngleRel('B', 30, 90);
+        brick.MoveMotorAngleRel('B', 55, 90);
         brick.WaitForMotor('B');
         % MOVE FORWARD UNTIL GREEN
-        brick.MoveMotor('AB', 30);
+        brick.MoveMotor('AB', 55);
     end
 
     % IF WALL NOT THERE, TURN
     if dist > 4
         pause(1);
-        brick.MoveMotorAngleRel('A', 30, 90);
+        brick.MoveMotorAngleRel('A', 55, 90);
         brick.WaitForMotor('A');
         pause(2);
-        brick.MoveMotor('AB', 30);
+        brick.MoveMotor('AB', 55);
     end
 
 end
