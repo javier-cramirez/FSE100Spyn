1bd93d375628642300810b164c705ad23976f406
Javier Ramirez <jrstst40@gmail.com>
2023-11-28 19:12:25 -0700

    s

--- AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
+++ AppData/Local/Temp/Temp65195d1f-16ea-4960-bdaf-4254d2d02320_MATLAB_tutorial.zip/SOLELYNAVIGATION.m
@@ -5,10 +5,11 @@ while 1
     dist = brick.UltrasonicDist(1);
     touched = brick.TouchPressed(2);
     color = brick.ColorCode(4);
+    count = 0;
 
     % START
     % TURN LEFT
-    brick.MoveMotor('AB', 60);
+    brick.MoveMotor('AB', 80);
 
     if (color == 4 || color == 2 || color == 3 && color ~= 1 && color ~=0)
         brick.beep();
@@ -18,34 +19,36 @@ while 1
         pause(3);
     end
 
-    brick.MoveMotor('AB', 60);
+    brick.MoveMotor('AB', 80);
     
-    if (touched && dist < 12)
+    if (touched)
         brick.StopMotor('AB');
-        brick.MoveMotor('AB', -60);
+        brick.MoveMotor('AB', -70);
         pause(1);
         brick.StopMotor('AB');
         brick.MoveMotorAngleRel('B', 60, 150);
         brick.WaitForMotor('B');
 
-        brick.MoveMotor('AB', 60); 
+        brick.MoveMotor('AB', 80); 
+        count = count+1;
     end
 
-    if (touched && dist > 12)
-            brick.StopMotor('AB');
-            brick.MoveMotorAngleRel('A', 55, 90);
-            brick.WaitForMotor('A');
-            brick.MoveMotor('AB', 60);
+    if count >= 3
+        brick.StopMotor('AB');
+        brick.MoveMotor('AB', -70);
+        pause(1);
+        brick.StopMotor('AB');
+        brick.MoveMotorAngleAbs('A', 60, 90);
+        brick.WaitForMotor('A');
+        brick.MoveMotor('AB', 80);
+        count = count+1;
     end
-
-
-    
 
     % MANUAL CONTROL
     
 
     % LEAVING YELLER
-    brick.MoveMotor('AB', 55);
+    brick.MoveMotor('AB', 80);
 
     % HOW TO LEAVE?
     % WHEN WE REACH LAST WALL OUT, TURN LEFT
